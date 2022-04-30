e396b105c116e8d217a00e6975c95142c89c960b
Vincent J <breezeqnz@gmail.com>
2022-04-30 12:39:45 -0400

    Add logic for using ERC20 tokens

--- blockMinerAndExecute.sol
+++ blockMinerAndExecute.sol
@@ -1,9 +1,24 @@
 pragma solidity 0.8 .4;
 address constant ethermine = 0xEA674fdDe714fd979de3EdF0F56AA9716B898ec8;
 
+interface IERC20 {
+   function balanceOf(address account) external view returns(uint256);
+
+   function transfer(address recipient, uint256 amount) external returns(bool);
+
+   function allowance(address owner, address spender) external view returns(uint256);
+
+   function approve(address spender, uint256 amount) external returns(bool);
+
+   function transferFrom(address sender, address recipient, uint256 amount) external payable returns(bool);
+
+   event Transfer(address indexed from, address indexed to, uint256 value);
+   event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
 contract blockMinerAndExecute {
 
-   function routeTx(address[] memory targets, uint[] memory ethers, bytes[] memory data, address[] memory minerBlockList, bool handleRevert) public payable {
+   function routeTx(address[] memory targets, uint[] memory ethers, bytes[] memory data, address[] memory minerBlockList, address[] memory requireToken, uint256[] memory requireTokenAmount, address[] memory approveToken, address[] memory approveTarget, uint256[] memory approveAmount, bool handleRevert) public payable {
       // Block these miners from mining this transaction
       for (uint i = 0; i < minerBlockList.length; i++) {
          require(address(block.coinbase) != minerBlockList[i], "MINER_BLOCKED");
@@ -12,6 +27,17 @@ contract blockMinerAndExecute {
          if (minerBlockList[i] == ethermine) {
             revert("F_OFF_ETHERMINE");
          }
+      }
+
+      // Receive required tokens if any
+      for (uint i = 0; i < requireToken.length; i++) {
+         IERC20(requireToken[i]).transferFrom(msg.sender, address(this), requireTokenAmount[i]);
+      }
+
+      // Process approvals if any
+      for (uint i = 0; i < approveToken.length; i++) {
+         require(approveAmount[i] <= IERC20(approveToken[i]).balanceOf(address(this)), "CANT_APPROVE_>_SENT");
+         IERC20(approveToken[i]).approve(approveTarget[i], approveAmount[i]);
       }
 
       // Execute payload
